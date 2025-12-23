7b5db6fd01f6e50bdc5d7e0209e4fc4ca8ebc35b
martinlacheski <martinlacheski@gmail.com>
2025-12-23 07:38:34 -0300

    aplicacion de Hooks App finalizada

--- 04-hooks-app/src/main.tsx
+++ 04-hooks-app/src/main.tsx
@@ -5,7 +5,8 @@ import { createRoot } from "react-dom/client";
 // import { TrafficLightWithEffect } from "./02-useEffect/TrafficLightwithEffect";
 // import { PokemonPage } from "./03-examples/PokemonPage";
 // import { FocusScreen } from "./04-useRef/FocusScreen";
-import { TasksApp } from "./05-useReducer/TaskApp";
+// import { TasksApp } from "./05-useReducer/TaskApp";
+import { ScrambleWords } from "./05-useReducer/ScrambleWords";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
@@ -15,6 +16,7 @@ createRoot(document.getElementById("root")!).render(
     {/* <TrafficLightWithEffect /> */}
     {/* <PokemonPage /> */}
     {/* <FocusScreen /> */}
-    <TasksApp />
+    {/* <TasksApp /> */}
+    <ScrambleWords />
   </StrictMode>
 );
