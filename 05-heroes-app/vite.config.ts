f62e8347a30122373bd954eb7b20992347a39ab5
martinlacheski <martinlacheski@gmail.com>
2025-12-24 12:04:06 -0300

    inicialización de tests en la aplicacion

--- 05-heroes-app/vite.config.ts
+++ 05-heroes-app/vite.config.ts
@@ -1,14 +1,11 @@
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+  plugins: [react()],
+  test: {
+    environment: "jsdom",
+    globals: true,
   },
 });
